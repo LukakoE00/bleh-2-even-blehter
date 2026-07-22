cd9f66f7122a7aa7af0fef57949ff7b063381c25
Luka <ljjk.loos@gmail.com>
2026-07-22 14:05:10 +0200

    Stop being an idiot

--- Neurotrauma/Lua/Scripts/Server/items.lua
+++ Neurotrauma/Lua/Scripts/Server/items.lua
@@ -675,7 +675,7 @@ end
 NT.ItemMethods.emptybloodpack = function(item, usingCharacter, targetCharacter, limb)
 	if item.Condition <= 0 then return end
 
-	if not usingCharacter.Character.IsHuman then return end
+	if not targetCharacter.Character.IsHuman then return end
 
 	if targetCharacter.Bloodloss <= 31 then
 		local success = HF.GetSkillRequirementMet(usingCharacter, "medical", 30)
